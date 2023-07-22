7c6368f8bbb5ae619e854c68300b35d28fbbfec7
AventixQ <jagoda.anna.janowska@gmail.com>
2023-07-22 17:49:01 +0200

    Fixes in the Amazon data set code

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
-from tqdm.notebook import tqdm
+from typing import Iterable, Dict
 
 #nltk.download('punkt')
 #nltk.download('averaged_perceptron_tagger')
@@ -16,73 +16,94 @@ from tqdm.notebook import tqdm
 
 plt.style.use('ggplot')#
 
+def read_data(filepath: str, num_rows: int) -> pd.DataFrame:
+    data = pd.read_csv(filepath)
+    data = data.head(num_rows)
+    return data
 
-#reading data and basic analysis
-data = pd.read_csv('./data/Reviews.csv')
-data = data.head(1000) #using 1000 rows
-#print(data.shape)
+def tokenize(text: str) -> Iterable[str]:
+    tokens = nltk.word_tokenize(text)
+    return tokens
 
-ax = data['Score'].value_counts().sort_index().plot(kind = 'bar',title = 'Count of Reviews',\
-                                               figsize = (10, 5))
-ax.set_xlabel('Review Stars')
-plt.show()
+def tagged(tokens: Iterable[str]) -> Iterable[tuple]:
+    tagged = nltk.pos_tag(tokens)
+    return tagged
 
-polarity = []
-sia = SentimentIntensityAnalyzer()
+def analyze_sentiment_for_given_txt(text: str) -> Dict[str, float]:
+    sia = SentimentIntensityAnalyzer()
+    sentiment_scores = sia.polarity_scores(text)
 
-for i in data['Id']:
-    tmp = []
-    tmp.append(i)
-    polarity.append(tmp)
-q = 0
-for i in data['Text']:
-    polarity[q].append(sia.polarity_scores(i))
-    q += 1
+    return sentiment_scores
 
-sentence_scores = {}
-for i in polarity:
-    sentence_scores[i[0]] = i[1]
+def calculate_sentiment_scores(data: pd.DataFrame) -> pd.DataFrame:
+    polarity = []
+    sia = SentimentIntensityAnalyzer()
 
-sentence_scores = pd.DataFrame(sentence_scores).T
+    for i in data['Id']:
+        tmp = []
+        tmp.append(i)
+        polarity.append(tmp)
 
-sentence_scores = sentence_scores.reset_index().rename(columns={'index':'Id'})
-#print(sentence_scores)
-sentence_scores = sentence_scores.merge(data, how = 'left')
-#print(sentence_scores['compound'])
-#print(sentence_scores.head())
+    q = 0
+    for i in data['Text']:
+        polarity[q].append(sia.polarity_scores(i))
+        q += 1
+
+    sentence_scores = {}
+    for i in polarity:
+        sentence_scores[i[0]] = i[1]
+
+    sentence_scores = pd.DataFrame(sentence_scores).T
+    sentence_scores = sentence_scores.reset_index().rename(columns={'index': 'Id'})
+    sentence_scores = sentence_scores.merge(data, how='left')
+    return sentence_scores
+
+def scatter_plot(data: pd.DataFrame):
+    plt.scatter(data['Score'], data['compound'], color='red', marker='.')
+    plt.xlabel('Score')
+    plt.ylabel('Compound')
+    plt.title('VADER results by Score')
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+
+def bar_plot(data: pd.DataFrame):
+    sentiment_means = data.groupby('Score')['compound'].mean()
+    stars = sentiment_means.index
+    plt.bar(stars, sentiment_means, align='center', alpha=0.7)
+    plt.xlabel('Score')
+    plt.ylabel('Average sentiment result')
+    plt.title('Distributions of VADER results by Star Review')
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+
+def grouped_bar_plot(data: pd.DataFrame):
+    fig, axs = plt.subplots(2, 2, figsize=(10, 10))
+    sns.barplot(data=data, x=data['Score'], y=data['compound'], ax=axs[0, 0])
+    sns.barplot(data=data, x=data['Score'], y=data['pos'], ax=axs[0, 1])
+    sns.barplot(data=data, x=data['Score'], y=data['neu'], ax=axs[1, 0])
+    sns.barplot(data=data, x=data['Score'], y=data['neg'], ax=axs[1, 1])
+    axs[0, 0].set_title("Compound")
+    axs[0, 1].set_title("Positive")
+    axs[1, 0].set_title("Neutral")
+    axs[1, 1].set_title("Negative")
+    plt.show()
+
+def main():
+    data = read_data('./data/Reviews.csv', 1000)
+    tokens = tokenize(data['Text'][50])
+    print(tokens)
+    print(tagged(tokens))
+    sentence_scores = calculate_sentiment_scores(data)
+    scatter_plot(sentence_scores)
+    bar_plot(sentence_scores)
+    grouped_bar_plot(sentence_scores)
+
+if __name__ == "__main__":
+    main()
 
 
-#WYKRES 1
-plt.scatter(sentence_scores['Score'], sentence_scores['compound'], color='red', marker='.')
-plt.xlabel('Score')
-plt.ylabel('Compound')
-plt.title('VADER results by Score')
-plt.grid(True)
-plt.tight_layout()
-plt.show()
-
-#WYKRES 2
-sentiment_means = sentence_scores.groupby('Score')['compound'].mean()
-stars = sentiment_means.index
-plt.bar(stars, sentiment_means, align='center', alpha=0.7)
-plt.xlabel('Score')
-plt.ylabel('Average sentiment result')
-plt.title('Distributions of VADER results by Star Review')
-plt.grid(True)
-plt.tight_layout()
-plt.show()
-
-#WYKRES 3
-fig, axs = plt.subplots(2,2,figsize=(10, 10))
-sns.barplot(data = sentence_scores, x = sentence_scores['Score'], y = sentence_scores['compound'], ax=axs[0, 0])
-sns.barplot(data = sentence_scores, x = sentence_scores['Score'], y = sentence_scores['pos'], ax=axs[0, 1])
-sns.barplot(data = sentence_scores, x = sentence_scores['Score'], y = sentence_scores['neu'], ax=axs[1, 0])
-sns.barplot(data = sentence_scores, x = sentence_scores['Score'], y = sentence_scores['neg'], ax=axs[1, 1])
-axs[0, 0].set_title("Compound")
-axs[0, 1].set_title("Positive")
-axs[1, 0].set_title("Neutral")
-axs[1, 1].set_title("Negative")
-plt.show()
 
 #example for seperate sentence
 '''
