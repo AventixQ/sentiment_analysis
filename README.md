24f64c850136e06f47708287d5bf47f1cff9364d
AventixQ <jagoda.anna.janowska@gmail.com>
2023-07-27 19:53:02 +0200

    Adding more functionalities to code and updating README

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
-# sentiment_analysis
+# Sentiment Analysis with VADER
+
+This project performs sentiment analysis on a dataset using VADER (Valence Aware Dictionary and sEntiment Reasoner) from the Natural Language Toolkit (NLTK) library. The dataset used for analysis is the Amazon reviews dataset.
+
+## Requirements
+
+- Python 3.6 or higher
+- pandas
+- numpy
+- matplotlib
+- seaborn
+- nltk
+- vader_lexicon
+
+## Installation
+
+1. Clone the repository to your local machine:
+
+git clone https://github.com/your_username/sentiment_analysis.git
+
+
+2. Install the required packages using pip:
+
+pip install -r requirements.txt
+
+
+3. Download the required NLTK data:
+
+Uncomment the following lines in the main.py file (if needed):
+
+#nltk.download('punkt')
+#nltk.download('averaged_perceptron_tagger')
+#nltk.download('maxent_ne_chunker')
+#nltk.download('words')
+#nltk.download('vader_lexicon')
+
+4. Run the main.py file:
+
+python main.py
+
+##Overview
+
+The sentiment analysis is performed using the VADER (Valence Aware Dictionary and sEntiment Reasoner) sentiment analysis tool. The code includes functions to read the dataset, tokenize the text, perform sentiment analysis, calculate sentiment scores, visualize the results, and create a CSV file with the actual and predicted sentiments. It also calculates various metrics such as precision, recall, F-measure, and accuracy based on the predicted sentiments.
+
+##Dataset
+
+The Amazon reviews dataset is used for this sentiment analysis. The dataset can be found at the following link: Amazon Reviews Dataset [https://www.kaggle.com/datasets/bittlingmayer/amazonreviews]
+
+##Results
+
+The code allows working with individual sentences or a specific database.
+
+For the entered text, it creates tokens and tags, as well as performs sentiment analysis.
+
+For more extensive databases the code generates various visualizations, including scatter plots and bar plots, to visualize the sentiment analysis results.
+
+It also creates CSV files with the actual and predicted sentiments for both 2-class (positive/negative) and 3-class (positive/negative/neutral) sentiment categories. Metrics such as precision, recall, F-measure, and accuracy are calculated and printed for each sentiment category.
+
+##Conclusion
+
+This project demonstrates the application of VADER sentiment analysis on the Amazon reviews dataset. It provides insights into the sentiment distribution and allows for the evaluation of the sentiment analysis performance using various metrics.
+
--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from typing import Iterable, Dict
 #nltk.download('vader_lexicon')
 
 plt.style.use('ggplot')
+sia = SentimentIntensityAnalyzer()
 
 def read_data(filepath: str, num_rows: int) -> pd.DataFrame:
     data = pd.read_csv(filepath)
@@ -36,8 +37,6 @@ def analyze_sentiment_for_given_txt(text: str) -> Dict[str, float]:
     return sentiment_scores
 
 def calculate_sentiment_scores(data: pd.DataFrame) -> pd.DataFrame:
-    sia = SentimentIntensityAnalyzer()
-
     #calculating sentiment scores for 'Text' column
     data['sentiment_scores'] = data['Text'].apply(sia.polarity_scores)
 
@@ -47,28 +46,6 @@ def calculate_sentiment_scores(data: pd.DataFrame) -> pd.DataFrame:
     data.drop(columns=['sentiment_scores'], inplace=True)
 
     return data
-
-'''polarity = []
-    sia = SentimentIntensityAnalyzer()
-
-    for i in data['Id']:
-        tmp = []
-        tmp.append(i)
-        polarity.append(tmp)
-
-    q = 0
-    for i in data['Text']:
-        polarity[q].append(sia.polarity_scores(i))
-        q += 1
-
-    sentence_scores = {}
-    for i in polarity:
-        sentence_scores[i[0]] = i[1]
-
-    sentence_scores = pd.DataFrame(sentence_scores).T
-    sentence_scores = sentence_scores.reset_index().rename(columns={'index': 'Id'})
-    sentence_scores = sentence_scores.merge(data, how='left')
-    return sentence_scores'''
 
 
 def scatter_plot(data: pd.DataFrame):
@@ -103,54 +80,152 @@ def grouped_bar_plot(data: pd.DataFrame):
     axs[1, 1].set_title("Negative")
     plt.show()
 
+def map_to_sentiment_category3(score: int):
+    if score in [1, 2]:
+        return -1  #Negative
+    elif score == 3:
+        return 0   #Neutral
+    elif score in [4, 5]:
+        return 1   #Positive
+
+def map_compound_to_sentiment_category3(compound_score: float):
+    if compound_score <= -0.25:
+        return -1  #Negative
+    elif compound_score >= 0.25:
+        return 1   #Positive
+    else:
+        return 0   #Neutral
+
+def map_to_sentiment_category2(score: int):
+    if score in [1, 2, 3]:
+        return -1  #Negative
+    elif score in [4, 5]:
+        return 1   #Positive
+
+def map_compound_to_sentiment_category2(compound_score: float):
+    if compound_score < 0:
+        return -1  #Negative
+    else:
+        return 1   #Positive
+
+#Creating csv file to compare predictions with actual data
+def create_csv(data: pd.DataFrame, size: int):
+    if size == 3:
+        data['actual_sentiment'] = data['Score'].apply(map_to_sentiment_category3)
+        data['predicted_sentiment'] = data['compound'].apply(map_compound_to_sentiment_category3)
+    elif size == 2:
+        data['actual_sentiment'] = data['Score'].apply(map_to_sentiment_category2)
+        data['predicted_sentiment'] = data['compound'].apply(map_compound_to_sentiment_category2)
+    #Create final csv and save
+    result_data = data[['actual_sentiment', 'predicted_sentiment']]
+    result_data.to_csv(f'./sentiment_results{size}.csv', index=False)
+
+    return result_data
+
+def calculate_precision(actual_sentiment, predicted_sentiment, label: int):
+    TP = sum((actual == label) and (predicted == label) for actual, predicted in zip(actual_sentiment, predicted_sentiment))
+    FP = sum((actual != label) and (predicted == label) for actual, predicted in zip(actual_sentiment, predicted_sentiment))
+
+    precision = TP / (TP + FP) if TP + FP != 0 else 0.0
+    return precision
+
+def calculate_recall(actual_sentiment: pd.DataFrame, predicted_sentiment: pd.DataFrame, label: int):
+    TP = sum((actual == label) and (predicted == label) for actual, predicted in zip(actual_sentiment, predicted_sentiment))
+    FN = sum((actual == label) and (predicted != label) for actual, predicted in zip(actual_sentiment, predicted_sentiment))
+
+    recall = TP / (TP + FN) if TP + FN != 0 else 0.0
+    return recall
+
+def calculate_accuracy(actual_sentiment: pd.DataFrame, predicted_sentiment: pd.DataFrame):
+    TP = sum((actual == predicted) for actual, predicted in zip(actual_sentiment, predicted_sentiment))
+    total_samples = len(actual_sentiment)
+    accuracy = TP / total_samples
+    return accuracy
+
+#Printing Confusion Matrix, Accurancy, Precision, Recall, F-measure
+def calculate_metrics(size = 3):
+    df = pd.read_csv(f'./sentiment_results{size}.csv')
+
+    actual_sentiment = df['actual_sentiment']
+    predicted_sentiment = df['predicted_sentiment']
+
+    print(f"METRICS FOR {size} CLASSES", end = "\n\n")
+
+    #Confusion matrix
+    if size == 3:
+        unique_labels = [-1, 0, 1]
+        confusion_matrix = np.zeros((3, 3), dtype=int)
+    elif size == 2:
+        unique_labels = [-1, 1]
+        confusion_matrix = np.zeros((2, 2), dtype=int)
+
+    for actual, predicted in zip(actual_sentiment, predicted_sentiment):
+        if actual in unique_labels and predicted in unique_labels:
+            confusion_matrix[unique_labels.index(actual)][unique_labels.index(predicted)] += 1
+
+    print("Confusion Matrix:")
+    print(confusion_matrix)
+
+    #Precision, Recall, Accurancy, F-measure
+    precision_per_class = {}
+    recall_per_class = {}
+    for label in unique_labels:
+        precision = calculate_precision(actual_sentiment, predicted_sentiment, label)
+        recall = calculate_recall(actual_sentiment, predicted_sentiment, label)
+        precision_per_class[label] = precision
+        recall_per_class[label] = recall
+
+    print("Accurancy:")
+    print(calculate_accuracy(actual_sentiment,predicted_sentiment))
+
+    print("Precision per Class:")
+    print(precision_per_class)
+    print("Average Precision:")
+    avg_precision = sum(precision_per_class.values())/size
+    print(avg_precision)
+
+    print("Recall per Class:")
+    print(recall_per_class)
+    print("Average Recall:")
+    avg_recall = sum(recall_per_class.values()) / size
+    print(avg_recall)
+
+    print("Average F-measure:")
+    print(2 * (avg_precision * avg_recall) / (avg_precision + avg_recall) if avg_precision + avg_recall != 0 else 0.0, end = "\n\n")
+
 def main():
     #dataset from: https://www.kaggle.com/datasets/bittlingmayer/amazonreviews
     data = read_data('./data/Reviews.csv', 1000)
-    #creating tokens and taggs
-    tokens = tokenize(data['Text'][50])
+
+    #Creating tokens and taggs and analyzing seperate sentence
+    #text = input('Put your sentence here: ') #input of your example to test
+    text = data['Text'][50] #example from Amazon data
+    tokens = tokenize(text)
+    print("Tokens of given sentence:")
     print(tokens)
+    print("Taggs of given sentence:")
     print(tagged(tokens))
-    #calculating sentiment scores with VADER
+    print("Sentiment analysis:")
+    print(sia.polarity_scores(text))
+
+    #Calculating sentiment scores with VADER
     sentence_scores = calculate_sentiment_scores(data)
-    #output of created data
+
+    #Output of created data
     print(sentence_scores)
-    #visualization of results
+
+    #Visualization of results
     scatter_plot(sentence_scores)
     bar_plot(sentence_scores)
     grouped_bar_plot(sentence_scores)
 
+    #Measures for negative, (neutral) and positive sentiment
+    create_csv(sentence_scores, 2) #csv for positive/negative
+    create_csv(sentence_scores, 3) #csv for positive/negative/neutral
+    calculate_metrics(2) #2 classes positive/negative, 2x2 matrix
+    calculate_metrics(3) #3 classes positive/negative/neutral, 3x3 matrix
+
 if __name__ == "__main__":
     main()
-
-
-
-#example for seperate sentence
-'''
-#NLTK
-#creating tokens
-example = data['Text'][50]
-#print(example)
-tokens = nltk.word_tokenize(example)
-#print(tokens[:10])
-#finding part of speach for each token
-tagged = nltk.pos_tag(tokens)
-#print(tagged)
-#putting into entities
-entities = nltk.chunk.ne_chunk(tagged_tokens = tagged)
-
-#VADER
-#each word is scored and combined to a total score
-
-#creating sentiment analysier object
-
-sia = SentimentIntensityAnalyzer()
-
-polar = sia.polarity_scores("I am so happy")
-print(polar)
-polar = sia.polarity_scores("I hate all my friends")
-print(polar)
-polar = sia.polarity_scores(example)
-print(polar)'''
-
 
 
